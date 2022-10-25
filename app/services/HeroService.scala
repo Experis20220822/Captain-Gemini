72434ce47b6d2eda9043ee84fe4b035efa7029c3
raw1697 <romel.w@hotmail.co.uk>
2022-10-25 16:30:42 +0100

    Hero Creation classes

---
+++ app/services/HeroService.scala
@@ -0,0 +1,18 @@
+package services
+
+import models.Hero
+import scala.util.Try
+
+trait HeroService {
+
+  def create(hero: Hero): Unit
+
+  def update(hero: Hero): Try[Hero]
+
+  def findById(id: Long): Option[Hero]
+
+  def findAll(): List[Hero]
+
+  def findByCountry(country: String): List[Hero]
+
+}
