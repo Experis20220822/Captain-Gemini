fb43aca5a0c4cccbfc4cf8f6f3191514f716ebdf
raw1697 <romel.w@hotmail.co.uk>
2022-10-28 13:32:29 +0100

    extra unit test

--- build.sbt
+++ build.sbt
@@ -105,6 +105,12 @@ lazy val itSettings = Defaults.itSettings ++ Seq(
 
 libraryDependencies ++= AppDependencies()
 
+libraryDependencies ++= Seq(
+  guice,
+  "com.dimafeng" %% "testcontainers-scala-scalatest" % testContainersScalaVersion % "test",
+  "com.dimafeng" %% "testcontainers-scala-mongodb" % testContainersScalaVersion % "test",
+
+)
 
 name := """captain-gemini"""
 organization := "com.captain-gemini"
--- test/services/MemoryHeroServiceSpec.scala
+++ test/services/MemoryHeroServiceSpec.scala
@@ -7,9 +7,23 @@ class MemoryHeroServiceSpec extends PlaySpec {
   "memoryHeroService" must {
     "return a list heros after I create a hero" in {
       val memoryHeroService = new MemoryHeroService()
-      val hero = Hero(10L, "BigR", "Heart is what wins battles", 1, 2,3,4)
+      val hero = Hero(10L, "BigR", "Heart is what wins battles", "https://www.wallpaperup.com/uploads/wallpapers/2013/06/14/102891/0fb6df085d56d99fec30784ea5f4772a-700.jpg", 2,3,4,4)
       memoryHeroService.create(hero)
       memoryHeroService.findAll().size mustBe (1)
+    }
+
+    "be able to update a Hero from the service" in {
+      val memoryStadiumService = new MemoryHeroService();
+      val hero = Hero(10L, "BigR", "Heart is what wins battles", "https://www.wallpaperup.com/uploads/wallpapers/2013/06/14/102891/0fb6df085d56d99fec30784ea5f4772a-700.jpg", 2,3,4,4)
+      memoryStadiumService.create(hero)
+      memoryStadiumService.findAll().size mustBe (1)
+      val maybeStadium = memoryStadiumService.findById(10L).get
+      val updated = hero.copy(name = "Birmingham")
+      println(updated)
+     // memoryStadiumService.update(updated)
+      val result = memoryStadiumService.findById(10L).get
+      result mustBe Hero(10L, "BigR", "Heart is what wins battles", "img", 2,3,4,4)
+      memoryStadiumService.findAll().size mustBe (1)
     }
 
   }
